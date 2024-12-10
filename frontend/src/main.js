2c1d20a79b05cb7c6544fdf1e435394e07449259
jieuny-coder <cherish08@kakao.com>
2024-12-10 10:43:04 +0900

    Vue.js 내부 구조 공부

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,10 +2,10 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router'  //만든 라우터 가져오기 
 
 const app = createApp(App)
 
-app.use(router)
+app.use(router)   // 라우터를 Vue앱에 등록
 
-app.mount('#app')
+app.mount('#app')  // Vue 앱을 index.html의 <div id="app">에 붙이기
